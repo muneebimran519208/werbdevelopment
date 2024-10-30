6064adf789b93fc25865de5fce9d254247feadb7
muneebimran519208 <muneebimran519208@gmail.com>
2024-10-30 14:47:28 +0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,17 @@
+let number=123
+let alphabet="abc"
+let one =String (number)
+let two =Number (alphabet)
+console.log(typeof one);
+console.log(typeof two);
+console.log(two);
+let x=true
+console.log(typeof x);
+let z=false
+console.log(typeof z);
+let t= String(x)
+console.log(typeof t)
+console.log(t);
+let i= String(z)
+console.log(typeof i)
+console.log(i);
---
+++ singer.js
@@ -0,0 +1,9 @@
+let x= "diljit"
+let y="karan"
+let z="dinjhik"
+if (x==y){
+  console.log("hello world");
+}
+else {
+    console.log("by world");  
+}
